cddb4d7dcdcad8a8e267f357701562a0bc52978b
parthibanpriyanka <41357177+parthibanpriyanka@users.noreply.github.com>
2018-07-23 23:17:25 -0700

    file added

---
+++ Assignment11/PrintAlternate.java
@@ -0,0 +1,19 @@
+package com.del;
+
+public class PrintAlternate {
+
+	public static void main(String[] args) {
+		int j=6;
+		for (int i=8; i<=13;i++) {
+			 {
+				if (j>0)
+					{
+					System.out.print(i +" "+ j +" ");
+					j--;
+					}
+			}
+		}
+
+	}
+
+}
